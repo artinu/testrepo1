075622eb1a3ee28524bc8bd04d3cadc9c679b6b9
piotr <artinu@live.com>
2016-03-24 09:56:53 +0300

    dd

--- testrepo1/testrepo1/Program.cs
+++ testrepo1/testrepo1/Program.cs
@@ -11,7 +11,7 @@ namespace testrepo1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("hello word");
+            Console.WriteLine("hello wordddd");
         }
     }
 }
